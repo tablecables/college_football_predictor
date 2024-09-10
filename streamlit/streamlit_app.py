74fd2439a07c735f40571cb93e60380d1df6c289
Colin Gaffney <colin.gaffney1@gmail.com>
2024-09-10 11:19:21 -0700

    streamlit loading

--- streamlit/streamlit_app.py
+++ streamlit/streamlit_app.py
@@ -24,11 +24,13 @@ if dashboard == "Weekly Win Probabilities":
 
 @st.cache_data
 def load_predictions():
-    return pd.read_parquet('../models/win_probability/predictions_2024_3.parquet')
+    file_path = os.path.join(os.path.dirname(__file__), 'predictions_2024_3.parquet')
+    return pd.read_parquet(file_path)
 
 @st.cache_data
 def load_logos():
-    return pd.read_csv('../src/utils/logos/logos.csv')
+    file_path = os.path.join(os.path.dirname(__file__), 'logos.csv')
+    return pd.read_csv(file_path)
 
 predictions = load_predictions()
 logos_df = load_logos()
